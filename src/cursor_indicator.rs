5a7ebba0a13846dfdf8557fa7c0a117e1a8faca1
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-01-24 03:47:33 -0400

    Add test for indicator rotation

--- src/cursor_indicator.rs
+++ src/cursor_indicator.rs
@@ -1,5 +1,5 @@
 use bevy::app::App;
-use bevy::prelude::{Added, Camera, Commands, Component, default, Entity, GlobalTransform, Plugin, Query, Res, SpriteBundle, Transform, Windows, With};
+use bevy::prelude::{Added, Camera, Commands, Component, default, Entity, GlobalTransform, Plugin, Query, Res, SpriteBundle, Transform, Vec3, Window, Windows, With};
 use bevy::asset::AssetServer;
 use bevy::hierarchy::BuildChildren;
 use bevy::math::{Quat, Vec2};
@@ -35,59 +35,51 @@ fn spawn_cursor_indicator(
 }
 
 fn my_cursor_system(
-    // need to get window dimensions
-    wnds: Res<Windows>,
-    // query to get camera transform
+    windows: Res<Windows>,
     q_camera: Query<(&Camera, &GlobalTransform)>,
-    player_q: Query<&GlobalTransform, With<Player>>,
-    mut cursor_q: Query<&mut Transform, With<CursorIndicator>>
+    mut indicator_q: Query<(&mut Transform, &GlobalTransform), With<CursorIndicator>>,
 ) {
-    // get the camera info and transform
-    // assuming there is exactly one main camera entity, so query::single() is OK
     let (camera, camera_transform) = q_camera.single();
 
-    // get the window that the camera is displaying to (or the primary window)
     let wnd = if let RenderTarget::Window(id) = camera.target {
-        wnds.get(id).unwrap()
+        windows.get(id).unwrap()
     } else {
-        wnds.get_primary().unwrap()
+        windows.get_primary().unwrap()
     };
 
-    // check if the cursor is inside the window and get its position
     if let Some(screen_pos) = wnd.cursor_position() {
-        // get the size of the window
-        let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
-
-        // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
-        let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
-
-        // matrix for undoing the projection and camera transform
-        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
-
-        // use it to convert ndc to world-space coordinates
-        let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
-
-        // reduce it to a 2D value
-        let cursor_pos: Vec2 = world_pos.truncate();
-
-        for player_transform in player_q.iter() {
-            let player_pos = player_transform.translation().truncate();
-            player_transform.forward();
-            let angle = (cursor_pos - player_pos).angle_between(player_pos);
-            let rotation = Quat::from_rotation_z(-angle);
-            for mut cursor_transform in cursor_q.iter_mut() {
-                cursor_transform.rotation = rotation;
-            }
+        let cursor_pos = get_cursor_translation(camera, camera_transform, wnd, screen_pos);
+        for (mut indicator_transform, cursor_global_transform) in indicator_q.iter_mut() {
+            let player_pos = cursor_global_transform.translation().truncate();
+            let rotation = get_rotation_from_to(player_pos, cursor_pos);
+            indicator_transform.rotation = rotation;
         }
     }
 }
 
+fn get_cursor_translation(camera: &Camera, camera_transform: &GlobalTransform, wnd: &Window, screen_pos: Vec2) -> Vec2 {
+    let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+    let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+    let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+    let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+    world_pos.truncate()
+}
+
+fn get_rotation_from_to(from: Vec2, to: Vec2) -> Quat {
+    let diff = to - from;
+    let angle = diff.y.atan2(diff.x);
+    let rotation = Quat::from_axis_angle(Vec3::new(0., 0., 1.), angle);
+    rotation
+}
+
 #[cfg(test)]
 mod indicator_cursor_test {
+    use bevy::math::DVec2;
     use super::*;
     use bevy::prelude::*;
-    use crate::player::Player;
-    use crate::test_utils::LoadTestPlugins;
+    use crate::camera::CameraPlugin;
+    use crate::player::{Player};
+    use crate::test_utils::{LoadTestPlugins, update};
 
     #[test]
     fn it_spawns_indicator_as_child_of_player() {
@@ -108,11 +100,43 @@ mod indicator_cursor_test {
         assert!(sprite.is_ok());
     }
 
+    #[test]
+    fn indicator_looks_at_mouse() {
+        let mut app = setup();
+        update(&mut app, 2);
+        let cursor_transform = app.world
+            .query_filtered::<&Transform, With<CursorIndicator>>()
+            .single(&app.world);
+        assert_eq!(cursor_transform.forward(), Vec3::new(0., 0., -1.));
+    }
+
     fn setup() -> App {
+        let windows = create_test_windows();
         let mut app = App::new();
         app.add_plugins(LoadTestPlugins)
-            .add_plugin(CursorIndicatorPlugin);
+            .add_plugin(CameraPlugin)
+            .add_plugin(CursorIndicatorPlugin)
+            .insert_resource(windows);
         let _ = app.world.spawn(Player).id();
         app
+    }
+
+    fn create_test_windows() -> Windows {
+        let mut windows = Windows::default();
+        let mut test_window = Window::new(
+            Default::default(),
+            &Default::default(),
+            100,
+            100,
+            1.0,
+            None,
+            None,
+        );
+        test_window.update_cursor_physical_position_from_backend(
+            // position from bottom left of windows
+            Option::from(DVec2::new(0., 50.))
+        );
+        windows.add(test_window);
+        windows
     }
 }
