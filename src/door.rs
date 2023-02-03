a49f72ac46d54a14126900b66b22019bcaa45b0d
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2023-02-03 08:31:23 -0400

    Add colliders to closed doors

--- src/door.rs
+++ src/door.rs
@@ -1,7 +1,7 @@
-use bevy::prelude::{App, Plugin, SpriteSheetBundle, Bundle, Component, TextureAtlasSprite, Changed, Query};
-use bevy_ecs_ldtk::{EntityInstance, LdtkEntity};
+use bevy::prelude::{App, Plugin, SpriteSheetBundle, Bundle, Component, TextureAtlasSprite, Changed, Query, Commands, Entity, BuildChildren, Mut, Children};
+use bevy_ecs_ldtk::prelude::{EntityInstance, LdtkEntity, RegisterLdtkObjects};
 use bevy_ecs_ldtk::ldtk::FieldValue;
-use bevy_ecs_ldtk::prelude::RegisterLdtkObjects;
+use bevy_rapier2d::prelude::Collider;
 use crate::interaction::Interaction;
 use crate::physics_bundle::{ObjectPhysicsBundle};
 
@@ -11,7 +11,7 @@ impl Plugin for DoorPlugin {
     fn build(&self, app: &mut App) {
         app
             .register_ldtk_entity::<DoorBundle>("Door")
-            .add_system(update_door_sprites);
+            .add_system(update_changed_doors);
     }
 }
 
@@ -61,21 +61,51 @@ impl From<EntityInstance> for Door {
     }
 }
 
-fn update_door_sprites(
-   mut door_query: Query<(&Door, &mut TextureAtlasSprite), Changed<Door>>
+fn update_changed_doors(
+    mut commands: Commands,
+    mut door_query: Query<(&Door, &mut TextureAtlasSprite, Entity, Option<&Children>), Changed<Door>>,
 ) {
-    for (door, mut sprite) in door_query.iter_mut() {
+    for (door, mut sprite, entity, children) in door_query.iter_mut() {
         match door.is_open() {
-            true => { sprite.index = 1 }
-            false => { sprite.index = 0 }
+            true => {
+                set_open_door_sprite(&mut sprite);
+                remove_blocking_collider(&mut commands, entity, children);
+            }
+            false => {
+                set_closed_door_sprite(&mut sprite);
+                add_blocking_collider(&mut commands, entity);
+            }
         }
     }
+}
+
+fn remove_blocking_collider(commands: &mut Commands, entity: Entity, children_r: Option<&Children>) {
+    if let Some(children) = children_r {
+        for &child in children.iter() {
+            commands.entity(entity).remove_children(&[child]);
+            commands.entity(child).despawn();
+        }
+    }
+}
+
+fn set_closed_door_sprite(sprite: &mut Mut<TextureAtlasSprite>) {
+    sprite.index = 0;
+}
+
+fn set_open_door_sprite(sprite: &mut Mut<TextureAtlasSprite>) {
+    sprite.index = 1;
+}
+
+fn add_blocking_collider(commands: &mut Commands, entity: Entity) {
+    commands.entity(entity).with_children(|parent| {
+        parent.spawn(Collider::cuboid(3.0, 1.0)); });
 }
 
 
 #[cfg(test)]
 mod doors_test {
-    use bevy::prelude::{App, TextureAtlasSprite, With};
+    use bevy::ecs::query::QueryEntityError;
+    use bevy::prelude::{App, Children, Entity, TextureAtlasSprite, With, Without};
     use bevy_rapier2d::prelude::*;
     use crate::door::{Door, DoorPlugin};
     use crate::interaction::Interaction;
@@ -127,13 +157,21 @@ mod doors_test {
     }
 
     #[test]
+    fn it_has_a_sensor_collider_by_default() {
+        let mut app = setup();
+        test_utils::update(&mut app, 3);
+        let door_with_collider_sensor_count = app.world
+            .query::<(&Door, &Sensor, &Collider, &RigidBody)>()
+            .iter(&app.world)
+            .len();
+        assert_eq!(door_with_collider_sensor_count, 1);
+    }
+
+    #[test]
     fn it_has_open_sprite_after_interacting() {
         let mut app = setup();
         test_utils::update(&mut app, 3);
-        let mut door = app.world
-            .query::<&mut Door>()
-            .single_mut(&mut app.world);
-        door.interact();
+        interact_with_door(&mut app);
         app.update();
         let sprite = app.world
             .query_filtered::<&TextureAtlasSprite, With<Door>>()
@@ -142,15 +180,57 @@ mod doors_test {
     }
 
     #[test]
-    fn it_has_a_collider_with_a_sensor() {
+    fn it_doesnt_have_blocking_collider_when_open() {
         let mut app = setup();
         test_utils::update(&mut app, 3);
-        let door_with_collider_sensor_count = app.world
-            .query::<(&Door, &Sensor, &Collider, &RigidBody)>()
-            .iter(&app.world)
-            .len();
-        assert_eq!(door_with_collider_sensor_count, 1);
+        let blocking_collider = get_blocking_collider(&mut app);
+        assert!(blocking_collider.is_err());
+    }
 
+    #[test]
+    fn it_has_blocking_collider_when_closed() {
+        let mut app = setup();
+        test_utils::update(&mut app, 3);
+        interact_with_door(&mut app);
+        app.update();
+        let blocking_collider = get_blocking_collider(&mut app);
+        assert!(blocking_collider.is_ok());
+    }
+
+    #[test]
+    fn it_doesnt_have_blocking_collider_when_reopened() {
+        let mut app = setup();
+        test_utils::update(&mut app, 3);
+        //close
+        interact_with_door(&mut app);
+        app.update();
+        //open
+        interact_with_door(&mut app);
+        app.update();
+        let blocking_collider = get_blocking_collider(&mut app);
+        assert!(blocking_collider.is_err());
+    }
+
+    fn interact_with_door(app: &mut App) {
+        let mut door = app.world
+            .query::<&mut Door>()
+            .single_mut(&mut app.world);
+        door.interact();
+    }
+
+    fn get_blocking_collider(app: &mut App) -> Result<&Collider, QueryEntityError> {
+        let mut blocking_collider = Err(QueryEntityError::NoSuchEntity(Entity::from_raw(0)));
+        let mut children_q = app.world
+            .query_filtered::<&Children, With<Door>>();
+        let mut collider_q = app.world
+            .query_filtered::<&Collider, Without<Sensor>>();
+        for children in children_q.iter(&app.world) {
+            for &child in children.iter() {
+                blocking_collider = collider_q
+                    .get(&app.world, child);
+            }
+        }
+        blocking_collider
     }
 
     fn setup() -> App {
